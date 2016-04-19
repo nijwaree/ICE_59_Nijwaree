e4c0c7f372fb7fd61cef1671f7a4ceadefa5de08
nijwaree <fonaugust@gmail.com>
2016-04-19 11:49:08 +0700

    computer lab2

--- app/src/main/java/ice_pbru/suksaovavimon/nijwaree/ice_59_nijwaree/MainActivity.java
+++ app/src/main/java/ice_pbru/suksaovavimon/nijwaree/ice_59_nijwaree/MainActivity.java
@@ -2,12 +2,24 @@ package ice_pbru.suksaovavimon.nijwaree.ice_59_nijwaree;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button AButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bindwidget();
+
+    }// main methd
+
+    private void bindwidget() {
+        AButton = (Button) findViewById(R.id.button);
+
     }
-}
+}// main class
